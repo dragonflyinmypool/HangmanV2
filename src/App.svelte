98d5a9f4258bf5f0b12e9b65ffda72327b06e93e
dragonflyinmypool <jheiney10@gmail.com>
2022-09-29 11:59:53 +0100

    "first commit"

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Prompt from "./lib/Prompt.svelte";
+  import WordDisplay from "./lib/WordDisplay.svelte";
+  import Letters from "./lib/Letters.svelte";
+  import Menu from "./lib/Menu.svelte";
+  import HangmanAnimation from "./lib/HangmanAnimation.svelte";
+</script>
+
+<h1>Hangman Game</h1>
+<Prompt />
+<HangmanAnimation />
+<WordDisplay />
+<Letters />
+<Menu />
